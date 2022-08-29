5e4d0207688ac88027a5fec9f9a1c05dcd830b61
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2022-08-29 12:58:21 +0200

    Initial commit

---
+++ TE21A_Luckext/Program.cs
@@ -0,0 +1,14 @@
+﻿string name;
+
+name = "Micke";
+
+Console.WriteLine("Vad heter du?");
+name = Console.ReadLine();
+
+Console.WriteLine($"Hej {name}");
+// Console.WriteLine(name);
+
+Console.WriteLine("Välkomna");
+
+
+Console.ReadLine();
